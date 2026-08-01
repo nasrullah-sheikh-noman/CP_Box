6a2e480b5ba53e9d9b4a96ab1e5b409f4b9b5310
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-08-01 08:34:42 +0600

    Update level_order_traversal.cpp

--- Binary_tree/level_order_traversal.cpp
+++ Binary_tree/level_order_traversal.cpp
@@ -1,8 +1,12 @@
 void level_order(Node* root) {
+  if(root==NULL) { 
+    cout << "No tree";
+    return;
+  }
   queue<Node *> q;
   q.push(root);
   while(!q.empty()) {
-    Node* p = q.front();
+    Node *p = q.front();
     q.pop();
     cout << p->val << " ";
     if(p->left) q.push(p->left);
