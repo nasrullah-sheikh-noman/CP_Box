30cdff47cb9f42358f95109734213e5ed5061614
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-08-07 12:44:18 +0600

    Create insert_at_max_heap.cpp

---
+++ Heap/insert_at_max_heap.cpp
@@ -0,0 +1,11 @@
+void insert_at_max_heap(vector<int> &v) {
+  int cur_idx = v.size()-1;
+  while(cur_idx!=0) {
+    int par_idx = (cur_idx-1)/2;
+    if(v[par_idx] < v[cur_idx]) {
+      swap(v[par_idx], v[cur_idx]);
+      cur_idx = par_idx;
+    } else
+      break;
+  }
+}
