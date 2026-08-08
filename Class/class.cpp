f981339009da56e280c1f2e1bb4855e50d85e2a2
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-08-08 12:49:29 +0600

    Create class.cpp

---
+++ Class/class.cpp
@@ -0,0 +1,11 @@
+class Student {
+  public:
+    string name;
+    int roll;
+    int marks;
+    Student(string name, int roll, int marks) {
+      this->marks = marks;
+      this->roll = roll;
+      this->name = name;
+    }
+};
