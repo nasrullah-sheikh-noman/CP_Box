b2293e36f2e6970e72b7e3df883dde44668d3a61
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-08-08 20:32:17 +0600

    Delete Algorithms/Marge_sort directory

--- Algorithms/Marge_sort/marge_sort.cpp
+++
@@ -1,8 +0,0 @@
-void margeSort(vector<int> &v, int left, int right) {
-  if(left<right) {
-    int mid = left + (right - left) / 2;
-    margeSort(v, left, mid);
-    margeSort(v, mid + 1, right);
-    marge(v, left, mid, right);
-  }
-}
