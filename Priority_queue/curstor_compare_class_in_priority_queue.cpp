0eebc596389afb4b74d609fb7ffff06e40710fd4
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-08-08 12:56:53 +0600

    Update curstor_compare_class_in_priority_queue.cpp

--- Priority_queue/curstor_compare_class_in_priority_queue.cpp
+++ Priority_queue/curstor_compare_class_in_priority_queue.cpp
@@ -1,6 +1,11 @@
 class cmp{
   public:
     bool operator()(Student l, Student r) {
-      return l.marks < r.marks;
+      if(l.marks < r.marks)
+        return true;
+      else if(l.marks > r.marks)
+        return false;
+      else
+        return l.roll > r.roll;
     }
 };
