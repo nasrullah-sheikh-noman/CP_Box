e3e6f3834619883ba012b0c476df660dc9ca9c32
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-08-17 23:40:20 +0600

    Create adjacency_matrix_of_directed_graph.cpp

---
+++ Data_Structure/Graph/adjacency_matrix_of_directed_graph.cpp
@@ -0,0 +1,19 @@
+void solve() {
+  int n, e; cin >> n >> e;
+  int v[n][n];
+  memset(v, 0, sizeof(v));
+  for(int i = 0; i < n; i++) {
+    v[i][i] = 1;
+  }
+  for(int i = 0; i < e; i++) {
+    int a, b;
+    cin >> a >> b;
+    v[a][b] = 1;
+  }
+  for (int i = 0; i < n; i++) {
+    for(int j = 0; j < n; j++) {
+      cout << v[i][j] << " ";
+    }
+    cout << nl;
+  }
+}
