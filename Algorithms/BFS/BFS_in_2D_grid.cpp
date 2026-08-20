931499c8bbef9842f3c4c09a0cbc5bf80473f7a5
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-08-20 18:08:49 +0600

    Create BFS_in_2D_grid.cpp

---
+++ Algorithms/BFS/BFS_in_2D_grid.cpp
@@ -0,0 +1,44 @@
+int n, m;
+char grid[10001][10001];
+bool vis[10001][10001];
+vector<pair<int, int>> moves = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+bool valid(int ci, int cj) {
+  if(ci<0 || ci>= n || cj<0 || cj>=m)
+    return false;
+  return true;
+}
+
+void BFS(int si, int sj) {
+  queue<pair<int, int>> q;
+  q.push({si, sj});
+  vis[si][sj] = true;
+  while(!q.empty()) {
+    pair<int, int> par = q.front();
+    int pi = par.first;
+    int pj = par.second;
+    q.pop();
+    cout << pi << " " << pj << nl;
+    for (int i = 0; i < 4; i++) {
+      int ci = pi + moves[i].first;
+      int cj = pj + moves[i].second;
+      if(valid(ci, cj) && !vis[ci][cj]) {
+        q.push({ci, cj});
+        vis[ci][cj] = true;
+      }
+    }
+  }
+}
+
+void solve() {
+  cin >> n >> m;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      cin >> grid[i][j];
+    }
+  }
+  int si, sj;
+  cin >> si >> sj;
+  memset(vis, false, sizeof(vis));
+  BFS(si, sj);
+}
